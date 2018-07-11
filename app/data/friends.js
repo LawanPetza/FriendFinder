4e2ba22409cdd626f7da533ef8414ca3e7763e49
Lawan Petza <Lawan.Petza@gmail.com>
2018-07-11 19:09:27 -0400

    modified js

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+var friends = [
+    {
+        name: "Jennifer",
+        image: "Jennifer.jpg",
+        scores: [1, 2 ,3, 4, 5, 1, 2, 3, 4, 5]
+    },
+    {
+        name: "Tom",
+        image: "Tom.jpg",
+        scores: [5, 4, 3, 2, 1, 1, 2, 3, 4, 5]
+    }
+
+]
+module.exports = friends;
